9c5f0e4b66d0b74f022b0e06617c17b3423d0f27
M Ajay Yadav <majay805@gmail.com>
2024-05-21 00:36:44 +0530

    created utils

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
-
+const connectDb = require("./utils/db")
 app.use(express.json());
 
 app.use("/api/auth",router);
 
-
-//! to keep everything simple I am using auth-router  
-// app.get("/",(req, resp) => {
-//     resp.status(200).send("welcome to homepage of the server ");
-// });
-
 const PORT = 5000;
-app.listen(PORT,() => {
-    console.log(`server is running at :${PORT}`)
 
-})
+connectDb().then(()=>{
+
+    app.listen(PORT,() => {
+        console.log(`server is running at :${PORT}`);
+    
+    });
+});
 
