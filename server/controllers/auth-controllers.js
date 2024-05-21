1d002018bd75f30ddebab3f39bdddb96f1b7a235
M Ajay Yadav <majay805@gmail.com>
2024-05-21 23:31:40 +0530

    created Database and model

--- server/controllers/auth-controllers.js
+++ server/controllers/auth-controllers.js
@@ -1,3 +1,5 @@
+const user = require("../models/user-model");
+
 const home = async (req, res) => {
 try {
     res
@@ -12,7 +14,13 @@ try {
 const register = async (req, res) => {
     try {
         console.log(req.body);
-        const data = req.body;
+        const {username, email, phone, password} = req.body;
+        const userExist = User.findOne({email: email});
+
+        if(userExist){
+            return res.status(400).json({msg:"email already exists"});
+        }
+        await User.create({username, email, phone, password});
         res.status(200).send({data})
     } catch (error) {
         res.status(400).send("their was error with register")
