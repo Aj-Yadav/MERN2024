5261f245c04dc97e1e20ad501d72de682f3cd4bc
M Ajay Yadav <majay805@gmail.com>
2024-05-29 20:04:46 +0530

    working on Register

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
         <ul>
           <li><NavLink exact to="/" activeClassName={styles.active}>Home</NavLink></li>
           <li><NavLink to="/about" activeClassName={styles.active}>About</NavLink></li>
-          <li><NavLink to="/contact" activeClassName={styles.active}>Contact</NavLink></li>
           <li><NavLink to="/services" activeClassName={styles.active}>Services</NavLink></li>
+          <li><NavLink to="/contact" activeClassName={styles.active}>Contact</NavLink></li>
           <li><NavLink to="/register" activeClassName={styles.active}>Register</NavLink></li>
           <li><NavLink to="/login" activeClassName={styles.active}>Login</NavLink></li>
         </ul>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,62 @@
+import React,{useState} from "react";
+import "./Register.css";
 const Register = () => {
+    const [user, setUser]=useState({
+        username:"",
+        email:"",
+        phone:"",
+        password:"",
+    });
     return(
-    <div>
-    <h1 className="NavC">hello home page</h1>
+    <>
+        <section>
+            <main>
+                <div className="section-registration">
+                    <div className="container grid grid-two-cols">
+                        <div className="registration-image">
+                            <img 
+                            src="/images/register.png"
+                            alt="a girl in trying to do registration"
+                            width="500px" height="500px"/>
+                        </div>
+                        {/* <div >Registration form</div> */}
+                        {/* <br /> */}
+                        <form className="main-hedding">
+                            <h2 className="heading2">Registration Form</h2>
+                            <div className="inlable">
+                                <label className="label" htmlFor="userName">username</label>
+                                <input  type="text"
+                                        name="username"
+                                        placeholder="enter your username"
+                                        id="username"
+                                        required autoComplete="off"/>
 
-</div>
-    )
-    }
+                                <label className="label" htmlFor="email">email</label>
+                                <input type="email" 
+                                        name="email"
+                                        placeholder="enter your email"
+                                        id="email"
+                                        required autoComplete="off"/>
+
+                                <label className="label" htmlFor="phone">phone</label>
+                                <input type="number"
+                                        name="phone"
+                                        placeholder="enter your number"
+                                        id="phone"
+                                        required autoComplete="off" />
+
+                                <label className="label" htmlFor="password">password</label>
+                                <input type="text"
+                                        name="password"
+                                        placeholder="enter your password"
+                                        id="password"
+                                        required autoComplete="off" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </main>
+        </section>
+    </>
+)}
 export default Register
