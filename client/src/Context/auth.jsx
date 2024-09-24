e82e5f8b935035b922c0e3a6bb5176e35ee46990
M-Ajay-Yadav <majay805@gmail.com>
2024-09-24 19:41:44 +0530

    latest commit of sep 2024

--- client/src/Context/auth.jsx
+++ client/src/Context/auth.jsx
@@ -1,110 +1,109 @@
-import { createContext , useContext, useEffect, useState} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-   const [token, setToken] = useState(localStorage.getItem("token"));
-   const [user, setUser] = useState("")
-   const [isLoading, setIsLoading] = useState(true)
-   const [service,setService] = useState([]);
-   const AuthorizationToken = `Bearer ${token}`
-   const API = "http://localhost:5000";
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [service, setService] = useState([]);
+  const AuthorizationToken = `Bearer ${token}`;
+  const API = "http://localhost:5000";
 
-   
-    
-    const storeTokenInLS = (serviceToken) => {
-        setToken(serviceToken);
-        return localStorage.setItem("token",serviceToken);
-    };
+  const storeTokenInLS = (serviceToken) => {
+    setToken(serviceToken);
+    return localStorage.setItem("token", serviceToken);
+  };
 
-    let isLoggedIn =!!token; 
-    // console.log("is logged in ",isLoggedIn)
-    //if token is "" then it taken false if their is any value in token is true
-    
-    const LogoutUser = () => {
-        setToken("");
-        return localStorage.removeItem('token')
-    }
-    useEffect(()=> {
-        userAuthentication();
-        // console.log("i am form use Effect")
+  let isLoggedIn = !!token;
+  // console.log("is logged in ",isLoggedIn)
+  //if token is "" then it taken false if their is any value in token is true
 
-    },[]);
-    const userAuthentication = async() => {
-        try{
-            const response = await fetch(`http://localhost:5000/api/auth/user`,{
-                method:"GET",
-                headers:{
-                    Authorization:AuthorizationToken
-                },
-
-            })
-            // console.log("I am from useAuth",response)
-
-            if(response.ok){
-                // console.log("response is ok");
-                const data = await response.json();
-                // console.log("user data of useAuthentication",data.userData);
-                setUser(data.userData);
-                setIsLoading(false);
-            }else{
-                console.log("error fetching user data")
-                setIsLoading(false);
-            }
-
-        }catch(error){
-            console.log("usecontext",error);
-        }
-    }
-
-    // console.log("form store",user)
-
-
-
-//serviceices List 
-   const getData = async() => {
-
+  const LogoutUser = () => {
+    setToken("");
+    return localStorage.removeItem("token");
+  };
+  useEffect(() => {
+    userAuthentication();
+    // console.log("i am form use Effect")
+  }, []);
+  const userAuthentication = async () => {
     try {
-        const response = await fetch("http://localhost:5000/api/data/service",{
-            method:"GET"
-    }
-        );
-        // if (Array.isArray(response.data)) {
-        if (response.ok) {
-            // console.log("ok started")
-            // setLists(response.data);
-            // console.log("ok ended")
-            const data = await response.json()
-            // console.log("data from auth",data)
-            const message = data.msg;
-            // console.log("resp of message",message)
-            setService(message)
-            // console.log("message data from auth after setservice",message)
+      const response = await fetch(`${API}/api/auth/user`, {
+        method: "GET",
+        headers: {
+          Authorization: AuthorizationToken,
+        },
+      });
+      // console.log("I am from useAuth",response)
 
-        }else{
-            console.error("unexpected response data",response.data)
-        }
-
+      if (response.ok) {
+        // console.log("response is ok");
+        const data = await response.json();
+        // console.log("user data of useAuthentication",data.userData);
+        setUser(data.userData);
+        setIsLoading(false);
+      } else {
+        console.log("error fetching user data");
+        setIsLoading(false);
+      }
     } catch (error) {
-        console.log(" serviceice list",error)
+      console.log("usecontext", error);
     }
-}
-    useEffect (() => {
-        getData();
-    },[])
-    
-    return (
-        <AuthContext.Provider value={{isLoggedIn, storeTokenInLS, LogoutUser,user,service, AuthorizationToken,API,isLoading}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  };
+
+  // console.log("form store",user)
+
+  //serviceices List
+  const getData = async () => {
+    try {
+      const response = await fetch(`${API}/api/data/service`, {
+        method: "GET",
+      });
+      // if (Array.isArray(response.data)) {
+      if (response.ok) {
+        // console.log("ok started")
+        // setLists(response.data);
+        // console.log("ok ended")
+        const data = await response.json();
+        // console.log("data from auth",data)
+        const message = data.msg;
+        // console.log("resp of message",message)
+        setService(message);
+        // console.log("message data from auth after setservice",message)
+      } else {
+        console.error("unexpected response data", response.data);
+      }
+    } catch (error) {
+      console.log(" serviceice list", error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        storeTokenInLS,
+        LogoutUser,
+        user,
+        service,
+        AuthorizationToken,
+        API,
+        isLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-
 export const useAuth = () => {
-    const authContextValue = useContext(AuthContext);
-    if(!authContextValue) {
-        throw new Error("useAuth used outside of the Provider");
-    }
-    return authContextValue;
-}
+  const authContextValue = useContext(AuthContext);
+  if (!authContextValue) {
+    throw new Error("useAuth used outside of the Provider");
+  }
+  return authContextValue;
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -10,42 +10,40 @@ const connectDb = require("./utils/db");
 const errorMiddleware = require("./middleware/error-middleware");
 const adminRoute = require("./router/admin-router");
 
-const url = require('url');
-const path = require('path');
-dotenv.config({ path: path.resolve(__dirname, './.env') });
+const url = require("url");
+const path = require("path");
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const BASE_URL = process.env.BASE_URL;
 if (!BASE_URL) {
-  console.error('Error: BASE_URL is not defined in the environment variables.');
+  console.error("Error: BASE_URL is not defined in the environment variables.");
   process.exit(1);
 }
 const parsedUrl = url.parse(BASE_URL);
-const basePath = parsedUrl.path;
+const basePath = parsedUrl.path;
 
 const corsoptions = {
-    origin:`*`,
-    method:"GET,POST,PUT,DELETE,PATCH,HEAD",
-    credentials:true,
-}
+  origin: `*`,
+  method: "GET,POST,PUT,DELETE,PATCH,HEAD",
+  credentials: true,
+};
 app.use(cors(corsoptions));
 app.use(express.json());
 
-app.use(`${basePath}api/auth`,authRoute);
-app.use(`${basePath}api/form`,contactRoute);
-app.use(`${basePath}api/data`,serviceRoute);
-app.use(`${basePath}api/admin`, adminRoute)
+app.use(`${basePath}api/auth`, authRoute);
+app.use(`${basePath}api/form`, contactRoute);
+app.use(`${basePath}api/data`, serviceRoute);
+app.use(`${basePath}api/admin`, adminRoute);
 
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 try {
-    connectDb().then(()=>{
-        app.listen(PORT,() => {
-            console.log(`server is running at :${PORT}`);
-        });
+  connectDb().then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at :${PORT}`);
     });
+  });
 } catch (error) {
-    console.log("server",error);
+  console.log("server", error);
 }
-
-
